517e18cb457b06c55c8fbe61901cd77e324f774c
kim.tum <kim.99.tumanyan@gmail.com>
2020-03-06 14:47:01 +0400

    added image repository

---
+++ FYT.DataAccess/Data/Repository/ImageRepository.cs
@@ -0,0 +1,26 @@
+﻿using FYT.DataAccess.Data.Repository.IRepository;
+using FYT.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FYT.DataAccess.Data.Repository
+{
+    public class ImageRepository : Repository<Image>, IImageRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ImageRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        
+        public void update(Image image)
+        {
+            var objFromDb = _db.Image.FirstOrDefault(i => i.Id == image.Id);
+
+            objFromDb.URL = image.URL; 
+        }
+    }
+}
