77200e0b8291577cd695956a6b66c40ed41f4473
ardo trtrian <ardotrtrian@gmail.com>
2020-03-09 13:37:53 +0400

    Added Foreign Keys to Course Table

--- FYT.Models/Course.cs
+++ FYT.Models/Course.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 using FYT.Models.Base;
 
@@ -13,12 +14,14 @@ namespace FYT.Models
         [StringLength(25)]
         public string Name { get; set; }
 
-        [Required]
         public int TutorId { get; set; }
+
+        [ForeignKey("TutorId")]
         public User Tutor { get; set; }
 
-        [Required]
         public int CategoryId { get; set; }
+
+        [ForeignKey("CategoryId")]
         public virtual Category Category { get; set; }
 
         [Required]
